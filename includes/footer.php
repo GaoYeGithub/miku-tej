aee6dbe94240070c7b1722d8659ca84dc866f98a
GaoYeGithub <152664000+GaoYeGithub@users.noreply.github.com>
2025-04-25 22:41:08 -0400

    cutie caty

--- includes/footer.php
+++ includes/footer.php
@@ -12,5 +12,7 @@
 </html>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="../oneko.js"></script>
+
 </body>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,4 @@
+
 <?php
 session_start();
 ?>
@@ -10,6 +11,5 @@ session_start();
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="../css/style.css" rel="stylesheet">
     <link href="../css/sub-page.css" rel="stylesheet">
-
 </head>
 <body>
